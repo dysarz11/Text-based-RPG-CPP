3d275909d0e3c31609a727040d78b7a51fda4a63
dysarz11 <justindysarz01@gmail.com>
2023-11-15 17:27:47 -0600

    make combat loop

--- Learn C++/main.cpp
+++ Learn C++/main.cpp
@@ -3,10 +3,31 @@
 #include <iostream>
 #include "dice.hpp"
 
+const short hpZero { 0 };
+int playerHealthPoints{ 50 };
+int orcHealthPoints{ 60 };
+
 int main() {
     
-    short D20 = rollD20();
-    short pairOfDice = roll2Dice();
-    
-    std::cout << D20 << std::endl << pairOfDice << std::endl;
+    while (playerHealthPoints > hpZero && orcHealthPoints > hpZero) {
+        short playerDamage = rollD20();
+        short orcDamage = roll2Dice();
+
+        orcHealthPoints -= playerDamage;
+        playerHealthPoints -= orcDamage;
+
+        std::cout << "The Orc took " << playerDamage << "DMG \n";
+        std::cout << "The Orc has " << orcHealthPoints << "HP left. \n" << std::endl;
+
+        std::cout << "You took " << playerDamage << "DMG! \n";
+        std::cout << "You have " << playerHealthPoints << "HP left. \n" << std:: endl << std::endl;
+
+    }
+    if (playerHealthPoints <= hpZero) {
+        std::cout << "You Died";
+    }
+    else if (orcHealthPoints <= hpZero) {
+        std::cout << "The orc was Killed!";
+    }
+    return 0;
 }
